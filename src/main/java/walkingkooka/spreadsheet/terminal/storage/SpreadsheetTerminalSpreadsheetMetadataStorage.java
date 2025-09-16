79eaf24df089dd697f15c766822b96adf122949b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-09-16 15:53:59 +1000

    SpreadsheetTerminalSpreadsheetMetadataStorage
    - Closes https://github.com/mP1/walkingkooka-spreadsheet-terminal/issues/5
    - SpreadsheetStorage: CRUD operations for Spreadsheet at "/spreadsheet/SpreadsheetId"

---
+++ src/main/java/walkingkooka/spreadsheet/terminal/storage/SpreadsheetTerminalSpreadsheetMetadataStorage.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.terminal.storage;
+
+import walkingkooka.collect.list.ImmutableList;
+import walkingkooka.net.header.MediaType;
+import walkingkooka.spreadsheet.SpreadsheetId;
+import walkingkooka.spreadsheet.SpreadsheetMediaTypes;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+import walkingkooka.spreadsheet.meta.store.SpreadsheetMetadataStore;
+import walkingkooka.storage.Storage;
+import walkingkooka.storage.StorageName;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.storage.StorageValue;
+import walkingkooka.storage.StorageValueInfo;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * A {@link Storage} that supports CRUD operations for {@link SpreadsheetMetadata}.
+ */
+final class SpreadsheetTerminalSpreadsheetMetadataStorage extends SpreadsheetTerminalStorage {
+
+    /**
+     * Singleton
+     */
+    final static SpreadsheetTerminalSpreadsheetMetadataStorage INSTANCE = new SpreadsheetTerminalSpreadsheetMetadataStorage();
+
+    private final static MediaType MEDIA_TYPE = SpreadsheetMediaTypes.OBJECT_SPREADSHEET_METADATA;
+
+    private SpreadsheetTerminalSpreadsheetMetadataStorage() {
+        super();
+    }
+
+    /**
+     * Loads the {@link SpreadsheetMetadata} with the given {@link SpreadsheetId}.
+     * <pre>
+     * /spreadsheet/SpreadsheetId
+     * </pre>
+     */
+    @Override
+    Optional<StorageValue> loadNonNull(final StoragePath path,
+                                       final SpreadsheetTerminalStorageContext context) {
+        final List<StorageName> storageNames = path.namesList();
+
+        switch (storageNames.size()) {
+            case 2:
+                return context.storeRepository()
+                    .metadatas()
+                    .load(
+                        context.convertOrFail(
+                            storageNames.get(1)
+                                .value(),
+                            SpreadsheetId.class
+                        )
+                    )
+                    .map(m -> StorageValue.with(
+                            path,
+                            Optional.of(m)
+                        ).setContentType(MEDIA_TYPE)
+                    );
+            default:
+                throw new IllegalArgumentException("Invalid path");
+        }
+    }
+
+    @Override
+    StorageValue saveNonNull(final StorageValue value,
+                             final SpreadsheetTerminalStorageContext context) {
+        final StoragePath path = value.path();
+        final List<StorageName> storageNames = path.namesList();
+
+        switch (storageNames.size()) {
+            case 1:
+                break;
+            case 2:
+                throw new IllegalArgumentException("Invalid path, SpreadsheetId not accepted");
+            default:
+                throw new IllegalArgumentException("Invalid path");
+        }
+
+        final SpreadsheetMetadata saved = context.storeRepository()
+            .metadatas()
+            .save(
+                context.convertOrFail(
+                    value.value()
+                        .orElseThrow(() -> new IllegalArgumentException("Missing " + SpreadsheetMetadata.class.getSimpleName())),
+                    SpreadsheetMetadata.class
+                )
+            );
+
+        return value.setPath(
+            StoragePath.ROOT.append(
+                StorageName.with(
+                    saved.getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID).toString()
+                )
+            )
+        ).setValue(
+            Optional.ofNullable(saved)
+        ).setContentType(MEDIA_TYPE);
+    }
+
+    /**
+     * Deletes the {@link SpreadsheetId} in the path.
+     */
+    @Override
+    void deleteNonNull(final StoragePath path,
+                       final SpreadsheetTerminalStorageContext context) {
+        final List<StorageName> names = path.namesList();
+        switch (names.size()) {
+            case 0:
+            case 1:
+                throw new IllegalArgumentException("Missing " + SpreadsheetId.class.getSimpleName());
+            case 2:
+                final SpreadsheetMetadataStore store = context.storeRepository()
+                    .metadatas();
+                store.delete(
+                    context.convertOrFail(
+                        names.get(1)
+                            .value(),
+                        SpreadsheetId.class
+                    )
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid path");
+        }
+    }
+
+    /**
+     * Supports listing using the provided {@link walkingkooka.spreadsheet.SpreadsheetName} pattern.
+     */
+    @Override
+    List<StorageValueInfo> listNonNull(final StoragePath path,
+                                       final int offset,
+                                       final int count,
+                                       final SpreadsheetTerminalStorageContext context) {
+        final List<StorageName> names = path.namesList();
+
+        final String name;
+
+        switch (names.size()) {
+            case 2:
+                name = context.convertOrFail(
+                    names.get(1)
+                        .value(),
+                    String.class
+                );
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid path");
+        }
+
+        return context.storeRepository()
+            .metadatas()
+            .findByName(
+                name,
+                offset,
+                count
+            ).stream()
+            .map(m ->
+                StorageValueInfo.with(
+                    StoragePath.ROOT.append(
+                        StorageName.with(
+                            m.getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID)
+                                .toString()
+                        )
+                    ),
+                    context.createdAuditInfo()
+                )
+            ).collect(ImmutableList.collector());
+    }
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return SpreadsheetMetadataStore.class.getSimpleName();
+    }
+}
---
+++ src/test/java/walkingkooka/spreadsheet/terminal/storage/SpreadsheetTerminalSpreadsheetMetadataStorageTest.java
@@ -0,0 +1,333 @@
+/*
+ * Copyright 2025 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.terminal.storage;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Either;
+import walkingkooka.net.AbsoluteUrl;
+import walkingkooka.net.email.EmailAddress;
+import walkingkooka.reflect.JavaVisibility;
+import walkingkooka.spreadsheet.SpreadsheetMediaTypes;
+import walkingkooka.spreadsheet.SpreadsheetName;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContextDelegator;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContexts;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataPropertyName;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataTesting;
+import walkingkooka.spreadsheet.meta.store.SpreadsheetMetadataStores;
+import walkingkooka.spreadsheet.security.store.SpreadsheetGroupStores;
+import walkingkooka.spreadsheet.security.store.SpreadsheetUserStores;
+import walkingkooka.spreadsheet.store.SpreadsheetCellRangeStores;
+import walkingkooka.spreadsheet.store.SpreadsheetCellReferencesStores;
+import walkingkooka.spreadsheet.store.SpreadsheetCellStores;
+import walkingkooka.spreadsheet.store.SpreadsheetColumnStores;
+import walkingkooka.spreadsheet.store.SpreadsheetLabelReferencesStores;
+import walkingkooka.spreadsheet.store.SpreadsheetLabelStores;
+import walkingkooka.spreadsheet.store.SpreadsheetRowStores;
+import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepositories;
+import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepository;
+import walkingkooka.spreadsheet.validation.form.store.SpreadsheetFormStores;
+import walkingkooka.storage.StoragePath;
+import walkingkooka.storage.StorageTesting;
+import walkingkooka.storage.StorageValue;
+import walkingkooka.storage.StorageValueInfo;
+import walkingkooka.storage.Storages;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class SpreadsheetTerminalSpreadsheetMetadataStorageTest implements StorageTesting<SpreadsheetTerminalSpreadsheetMetadataStorage, SpreadsheetTerminalStorageContext>,
+    SpreadsheetMetadataTesting {
+
+    @Test
+    public void testLoadMissingSpreadsheetMetadata() {
+        final TestSpreadsheetTerminalStorageContext context = new TestSpreadsheetTerminalStorageContext();
+
+        final StoragePath path = StoragePath.parse("/404");
+
+        this.loadAndCheck(
+            this.createStorage(),
+            path,
+            context
+        );
+    }
+
+    @Test
+    public void testLoad() {
+        final TestSpreadsheetTerminalStorageContext context = new TestSpreadsheetTerminalStorageContext();
+
+        final SpreadsheetMetadata metadata = context.storeRepository()
+            .metadatas()
+            .save(METADATA_EN_AU);
+
+        final StoragePath path = StoragePath.parse("/" + metadata.getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID));
+
+        this.loadAndCheck(
+            this.createStorage(),
+            path,
+            context,
+            StorageValue.with(
+                path,
+                Optional.of(metadata)
+            ).setContentType(SpreadsheetMediaTypes.OBJECT_SPREADSHEET_METADATA)
+        );
+    }
+
+    @Test
+    public void testSaveWithStoragePathIncludingSpreadsheetIdFails() {
+        final TestSpreadsheetTerminalStorageContext context = new TestSpreadsheetTerminalStorageContext();
+
+        final IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> this.createStorage()
+                .save(
+                    StorageValue.with(
+                        StoragePath.parse("/1"),
+                        Optional.of(METADATA_EN_AU)
+                    ),
+                    context
+                )
+        );
+
+        this.checkEquals(
+            "Invalid path, SpreadsheetId not accepted",
+            thrown.getMessage()
+        );
+    }
+
+    @Test
+    public void testSave() {
+        final TestSpreadsheetTerminalStorageContext context = new TestSpreadsheetTerminalStorageContext();
+
+        final SpreadsheetMetadata metadata = context.storeRepository()
+            .metadatas()
+            .save(METADATA_EN_AU);
+
+        this.saveAndCheck(
+            this.createStorage(),
+            StorageValue.with(
+                StoragePath.ROOT,
+                Optional.of(metadata)
+            ),
+            context,
+            StorageValue.with(
+                StoragePath.parse("/" + metadata.getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID)),
+                Optional.of(metadata)
+            ).setContentType(SpreadsheetMediaTypes.OBJECT_SPREADSHEET_METADATA)
+        );
+    }
+
+    @Test
+    public void testDelete() {
+        final TestSpreadsheetTerminalStorageContext context = new TestSpreadsheetTerminalStorageContext();
+
+        final SpreadsheetMetadata metadata = context.storeRepository()
+            .metadatas()
+            .save(METADATA_EN_AU);
+
+        final SpreadsheetTerminalSpreadsheetMetadataStorage storage = this.createStorage();
+        final StoragePath path = StoragePath.parse("/" + metadata.getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID));
+
+        storage.delete(
+            path,
+            context
+        );
+
+        this.loadAndCheck(
+            storage,
+            path,
+            context
+        );
+    }
+
+    @Test
+    public void testList() {
+        final TestSpreadsheetTerminalStorageContext context = new TestSpreadsheetTerminalStorageContext();
+
+        final SpreadsheetTerminalSpreadsheetMetadataStorage storage = this.createStorage();
+
+        final StorageValue value1 = storage.save(
+            StorageValue.with(
+                StoragePath.ROOT,
+                Optional.of(
+                    METADATA_EN_AU.set(
+                        SpreadsheetMetadataPropertyName.SPREADSHEET_NAME,
+                        SpreadsheetName.with("Hello1")
+                    )
+                )
+            ),
+            context
+        );
+
+        final StorageValue value2 = storage.save(
+            StorageValue.with(
+                StoragePath.ROOT,
+                Optional.of(
+                    METADATA_EN_AU.set(
+                        SpreadsheetMetadataPropertyName.SPREADSHEET_NAME,
+                        SpreadsheetName.with("Hello2")
+                    )
+                )
+            ),
+            context
+        );
+
+        final StorageValue value3 = storage.save(
+            StorageValue.with(
+                StoragePath.ROOT,
+                Optional.of(
+                    METADATA_EN_AU.set(
+                        SpreadsheetMetadataPropertyName.SPREADSHEET_NAME,
+                        SpreadsheetName.with("Different3")
+                    )
+                )
+            ),
+            context
+        );
+
+        final StoragePath path = StoragePath.parse("/Hello");
+
+        this.listAndCheck(
+            this.createStorage(),
+            path,
+            0,
+            2,
+            context,
+            StorageValueInfo.with(
+                StoragePath.parse(
+                    "/" + (
+                        (SpreadsheetMetadata) value1.value()
+                            .get()
+                    ).getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID)
+                ),
+                context.createdAuditInfo()
+            ),
+            StorageValueInfo.with(
+                StoragePath.parse(
+                    "/" + (
+                        (SpreadsheetMetadata) value2.value()
+                            .get()
+                    ).getOrFail(SpreadsheetMetadataPropertyName.SPREADSHEET_ID)
+                ),
+                context.createdAuditInfo()
+            )
+        );
+    }
+
+    @Override
+    public SpreadsheetTerminalSpreadsheetMetadataStorage createStorage() {
+        return SpreadsheetTerminalSpreadsheetMetadataStorage.INSTANCE;
+    }
+
+    @Override
+    public SpreadsheetTerminalStorageContext createContext() {
+        return SpreadsheetTerminalStorageContexts.fake();
+    }
+
+    static class TestSpreadsheetTerminalStorageContext extends FakeSpreadsheetTerminalStorageContext implements SpreadsheetEngineContextDelegator {
+
+        @Override
+        public SpreadsheetStoreRepository storeRepository() {
+            return this.engineContext.storeRepository();
+        }
+
+        @Override
+        public SpreadsheetMetadata spreadsheetMetadata() {
+            return this.engineContext.spreadsheetMetadata();
+        }
+
+        @Override
+        public <T> Either<T, String> convert(Object o, Class<T> aClass) {
+            return this.engineContext.convert(o, aClass);
+        }
+
+        @Override
+        public boolean canConvert(final Object value,
+                                  final Class<?> type) {
+            return this.engineContext.canConvert(
+                value,
+                type
+            );
+        }
+
+        @Override
+        public LocalDateTime now() {
+            return this.engineContext.now();
+        }
+
+        @Override
+        public Optional<EmailAddress> user() {
+            return this.engineContext.user();
+        }
+
+        @Override
+        public SpreadsheetEngineContext spreadsheetEngineContext() {
+            return this.engineContext;
+        }
+
+        private final SpreadsheetEngineContext engineContext = BasicSpreadsheetTerminalStorageContext.with(
+            SpreadsheetEngineContexts.basic(
+                AbsoluteUrl.parseAbsolute("https://example.com"),
+                METADATA_EN_AU.set(
+                    SpreadsheetMetadataPropertyName.LOCALE,
+                    LOCALE
+                ),
+                SpreadsheetStoreRepositories.basic(
+                    SpreadsheetCellStores.fake(),
+                    SpreadsheetCellReferencesStores.fake(),
+                    SpreadsheetColumnStores.fake(),
+                    SpreadsheetFormStores.fake(),
+                    SpreadsheetGroupStores.fake(),
+                    SpreadsheetLabelStores.fake(),
+                    SpreadsheetLabelReferencesStores.fake(),
+                    SpreadsheetMetadataStores.treeMap(
+                        METADATA_EN_AU,
+                        NOW
+                    ),
+                    SpreadsheetCellRangeStores.fake(),
+                    SpreadsheetCellRangeStores.fake(),
+                    SpreadsheetRowStores.fake(),
+                    Storages.fake(),
+                    SpreadsheetUserStores.fake()
+                ),
+                SpreadsheetMetadataPropertyName.SCRIPTING_FUNCTIONS,
+                SpreadsheetMetadataTesting.ENVIRONMENT_CONTEXT,
+                SpreadsheetMetadataTesting.LOCALE_CONTEXT,
+                SpreadsheetMetadataTesting.TERMINAL_CONTEXT,
+                SpreadsheetMetadataTesting.SPREADSHEET_PROVIDER,
+                SpreadsheetMetadataTesting.PROVIDER_CONTEXT
+            ),
+            TERMINAL_CONTEXT
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<SpreadsheetTerminalSpreadsheetMetadataStorage> type() {
+        return SpreadsheetTerminalSpreadsheetMetadataStorage.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
