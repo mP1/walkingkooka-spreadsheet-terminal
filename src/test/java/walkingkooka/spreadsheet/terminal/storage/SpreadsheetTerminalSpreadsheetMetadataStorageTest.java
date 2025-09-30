6bdaa76a2395ce7770d8d8b2b93dbebb6c933a7e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-09-30 17:27:37 +1000

    https://github.com/mP1/walkingkooka-spreadsheet/pull/7781 SpreadsheetEngineContext.setSpreadsheetEngineContextMode was spreadsheetEngineContext
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/7781
    - SpreadsheetEngineContext.setSpreadsheetEngineContextMode was spreadsheetEngineContext

--- src/test/java/walkingkooka/spreadsheet/terminal/storage/SpreadsheetTerminalSpreadsheetMetadataStorageTest.java
+++ src/test/java/walkingkooka/spreadsheet/terminal/storage/SpreadsheetTerminalSpreadsheetMetadataStorageTest.java
@@ -31,6 +31,7 @@ import walkingkooka.spreadsheet.compare.provider.SpreadsheetComparatorAliasSet;
 import walkingkooka.spreadsheet.convert.provider.SpreadsheetConvertersConverterProviders;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngineContextDelegator;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContextMode;
 import walkingkooka.spreadsheet.engine.SpreadsheetEngineContexts;
 import walkingkooka.spreadsheet.export.provider.SpreadsheetExporterAliasSet;
 import walkingkooka.spreadsheet.expression.SpreadsheetExpressionFunctions;
@@ -339,7 +340,7 @@ public final class SpreadsheetTerminalSpreadsheetMetadataStorageTest implements 
 
             return SpreadsheetEngineContexts.basic(
                 metadata,
-                SpreadsheetMetadataPropertyName.SCRIPTING_FUNCTIONS,
+                SpreadsheetEngineContextMode.SCRIPTING,
                 SpreadsheetContexts.basic(
                     AbsoluteUrl.parseAbsolute("https://example.com"),
                     id,
